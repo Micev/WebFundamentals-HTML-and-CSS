4cfe4a9119675349374cac99472f3921bdfc5edc
Georgi <Micev>
2014-07-03 21:49:19 +0300

    Homework

---
+++ Semantic HTML/LoveJavaScript.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Micev on 26.6.2014 г..
+ */
+var ctx = document.getElementById("myCanvas").getContext("2d");
+ctx.font = " 33px Tahoma";
+ctx.textAlign = "center";
+ctx.fillText("I love JavaScript", 135, 50);
+ctx.fillStyle = "#F1DA4E";
+ctx.fillRect(67.5, 80, 135, 135);
+ctx.font = "bold 85px Arial";
+ctx.fillStyle = "#30322e";
+ctx.fillText("JS", 145, 205);
